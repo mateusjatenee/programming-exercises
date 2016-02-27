feeb26f38cde0a20bc82cba4cdde04b5e4e18ae9
Mateus Guimarães <mateus.jatene@gmail.com>
2016-02-26 23:33:32 -0300

    php solution

---
+++ project-euler/001/php.php
@@ -0,0 +1,15 @@
+<?php
+
+function sumAllMultiples($limit)
+{
+    $sum = 0;
+    foreach (range(0, $limit - 1) as $i) {
+        if ($i % 3 == 0 || $i % 5 == 0) {
+            $sum += $i;
+        }
+    }
+
+    return $sum;
+}
+
+var_dump(sumAllMultiples(1000));
