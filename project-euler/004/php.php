454bc9dbd98f8beaf5d523149180d64adc25f9ae
Mateus Guimarães <mateus.jatene@gmail.com>
2016-02-28 03:25:12 -0300

    non optimized php solution for problem 4

---
+++ project-euler/004/php.php
@@ -0,0 +1,28 @@
+<?php
+
+function solveIt()
+{
+    $n = 999 * 999;
+
+    while ($n > 100000) {
+        $str = (string) $n;
+        $reverseStr = strrev($str);
+
+        if ($str == $reverseStr) {
+            $sqrt = sqrt($n);
+
+            $divisor = floor($sqrt);
+
+            while ($n % $divisor !== 0 && $divisor >= 100 && $n / $divisor <= 999) {
+                $divisor--;
+            }
+
+            if ($n % $divisor == 0 && $divisor >= 100 && $n / $divisor <= 999) {
+                return $n;
+            }
+        }
+
+    }
+}
+
+var_dump(solveit());
