26c3a4c5eb085d9078df1063fddcc09f632d861f
Mateus Guimarães <mateus.jatene@gmail.com>
2016-02-28 04:31:19 -0300

    problem 007 JS solution

---
+++ project-euler/007/javascript.js
@@ -0,0 +1,42 @@
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+// What is the 10 001st prime number?
+
+function isPrime(number) {
+	if(number % 2 == 0) {
+		return (number == 2);
+	} else {
+		var sqrt = Math.sqrt(number);
+
+		for(var i = 3; i <= sqrt; i += 2) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+
+function solveIt(limit) {
+	var primes = [2];
+	var i = 3;
+
+	while(primes.length < limit) {
+		if(isPrime(i)) {
+			primes.push(i);
+			i++;
+			continue;
+		}
+
+		i++;
+
+
+	}
+
+	return primes;
+}	
+
+var result = solveIt(10001);
+
+console.log(result[10000]);
