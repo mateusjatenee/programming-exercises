fd1508b9b05f902edf254d2a64ffe7f0c331c52c
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-01 03:53:08 -0300

    problem 4 php solution

---
+++ project-euler/006/php.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution
+{
+
+    public function __construct($limit)
+    {
+        $this->limit = $limit;
+    }
+
+    public function sumOfTheSquares()
+    {
+        $sum = 0;
+        for ($i = 0; $i < $this->limit; $i++) {
+            $sum += $i ** 2;
+        }
+        return $sum;
+    }
+
+    public function squareOfTheSum()
+    {
+        $sum = 0;
+        for ($i = 0; $i < $this->limit; $i++) {
+            $sum += $i;
+        }
+        return $sum ** 2;
+    }
+}
+
+$solution = new Solution(100);
+$result = $solution->squareOfTheSum() - $solution->sumOfTheSquares();
+
+var_dump($result);
