e4f32dcd41c362c241cc203b8cfb2992b77d911d
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-03 22:47:54 -0300

    max product of three solution

---
+++ codility/sorting/MaxProductOfThree.js
@@ -0,0 +1,13 @@
+function solution(a) {
+	var length = a.length;
+
+	a.sort(function(a, b) {
+		return a - b;
+	});
+
+	return Math.max(a[0] * a[1] * a[length - 1], a[length - 3] * a[length - 2] * a[length - 1]);
+}
+
+var array = [-3, 1, 2, -2, 5, 6];
+
+console.log(solution(array));
