ff510309ef6057de6925fb3fe5bd048c1bf2afe3
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-03 22:17:52 -0300

    codility distinct solution

---
+++ codility/sorting/distinct.js
@@ -0,0 +1,21 @@
+function solution(a) {
+	if(a.length == 0) return 0;
+
+	sorted = a.sort();
+	var last = 0;
+	var sum = 1;
+
+	for(var i = 1; i < sorted.length; i++) {
+		if(sorted[i] == sorted[i-1]) {
+			continue;
+		} else {
+			sum ++;
+		}
+	}
+
+	return sum;
+}
+
+
+var array = [2, 1, 3, 2, 3, 1];
+console.log(solution(array));
