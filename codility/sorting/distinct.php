a4a828d44a82ea44fbc8a9d349406ccf7a53a26f
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-03 23:56:14 -0300

    php solution for distinct

---
+++ codility/sorting/distinct.php
@@ -0,0 +1,27 @@
+<?php
+
+function solution($a)
+{
+    if (count($a) == 0) {
+        return 0;
+    }
+
+    $length = sizeof($a);
+
+    sort($a);
+
+    $sum = 1;
+
+    for ($i = 1; $i < $length; $i++) {
+        if ($a[$i] == $a[$i - 1]) {
+            continue;
+        } else {
+            $sum += 1;
+        }
+    }
+
+    return $sum;
+}
+
+$array = [2, 1, 3, 2, 3, 1];
+var_dump(solution($array));
