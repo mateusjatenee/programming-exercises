4d8e2f78280c389c63d7ef0648561110dd6927ce
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-11 14:36:44 -0300

    binary search implementation in php

---
+++ algorithms/binary_search/binary_search.php
@@ -0,0 +1,25 @@
+<?php
+
+function binarySearch($array, $n)
+{
+    $min = 0;
+    $max = sizeof($array) - 1;
+
+    while ($min <= $max) {
+        $avg = (int) ($max + $min) / 2;
+
+        if ($array[$avg] === $n) {
+            return $avg;
+        } elseif ($array[$avg] < $n) {
+            $min = $avg + 1;
+        } else {
+            $max = $avg - 1;
+        }
+    }
+
+    return -1;
+}
+
+$primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var_dump(binarySearch($primes, 97));
