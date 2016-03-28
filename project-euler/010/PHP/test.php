26602ed28aeae1fb833afa3757e0df8e2183b417
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-28 05:36:04 -0300

    adds test

---
+++ project-euler/010/PHP/test.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'solution.php';
+
+/**
+ *
+ */
+class TestSolution extends PHPUnit_Framework_TestCase
+{
+    public function testSolutionIsCorrect()
+    {
+        $limit = 2e6;
+        $expected = 142913828922;
+        $this->assertEquals(solution($limit), $expected);
+    }
+}
